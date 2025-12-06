e096435ba88515ad88361e3d6aa5d1811e41c2d9
Pragya Sharma <pragya21sharma@gmail.com>
2025-12-06 01:36:44 -0500

    Update backend URL for production

--- capstone-ui/src/apiClient.js
+++ capstone-ui/src/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
+  process.env.REACT_APP_API_BASE_URL || "https://capstone-project-81fb.onrender.com";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
